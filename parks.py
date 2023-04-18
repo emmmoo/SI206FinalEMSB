6b76dfbff5e1c8feac8a07f72a831b242e868158
Emma Moore <emmamoore@Emmas-MacBook-Air-108.local>
2023-04-18 14:43:51 -0400

    25 items tables

--- parks.py
+++ parks.py
@@ -37,7 +37,25 @@ def update_table(curr, conn, add):
         amentities = item["properties"]["DESCRIP"]
         curr.execute("""INSERT OR IGNORE INTO park_table (id, parkname, zipcode, city, region, type, amentities) VALUES (?, ?, ?, ?, ?, ?, ?)""", (park_id, park_name, zipcode, city, region, type, amentities))
     conn.commit()
-
+def library_table(curr, conn): 
+    curr.execute("CREATE TABLE IF NOT EXISTS library (library_id INTERGAR PRIMARY KEY, libraryname TEXT, zipcode INTEGER)")
+    conn.commit()
+    #created the library table but have not filled it yet 
+def fill_library(curr, conn, add): 
+    start = 0 + add 
+    limit = 25 + add
+    library_response = requests.get("https://services.arcgis.com/RmCCgQtiZLDCtblq/arcgis/rest/services/Public_Library_Facilities/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json")
+    items = library_response.json()
+    #iterate through the park info and add to dictionary
+    for item in items["features"][start:limit]: 
+        library_id = item["attributes"]["ObjectId"]
+        #print(library_id)
+        library_name = item["attributes"]["Library_Name"]
+        zipcode = item["attributes"]["Zip_Code"]
+        curr.execute("""INSERT OR IGNORE INTO park_table (library_id, library, zipcode) VALUES (?, ?, ?)""", (library_id, library_name, zipcode))
+    conn.commit()
+#create a statement to join the tables where the zipcodes are equal  
+# maybe I also need to create a table that for each zip code has the number of parks and libraries, that would be good to turn into a visualisation as well 
 
 def main():
     curr,conn = set_database("parks.db")
@@ -59,6 +77,24 @@ def main():
         update_table(curr, conn, 100)
     elif 125 <= length <150: 
         update_table(curr, conn, 125)
+#call the libary table and make sure it only adds 25 in at a time 
+    library_table(curr, conn)
+    curr.execute("SELECT COUNT(library_id) FROM library")
+    conn.commit()
+    linfo = curr.fetchall()
+    length = linfo[0][0]
+    if length < 25: 
+        fill_library(curr, conn, 0)
+    elif 25 <= length < 50: 
+        fill_library(curr, conn, 25)
+    elif 50 <= length < 75: 
+        fill_library(curr, conn, 50)
+    elif 75 <= length < 100: 
+        fill_library(curr, conn, 75)
+    elif 100 <= length < 125: 
+        fill_library(curr, conn, 100)
+    elif 125 <= length <150: 
+        fill_library(curr, conn, 125)
 
 
     
